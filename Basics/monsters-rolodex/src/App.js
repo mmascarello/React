5301dc3405857d26b229a25ca2cb25d170e4ce93
Maximiliano Mascarello <48534691+mmascarello@users.noreply.github.com>
2021-09-16 16:41:49 -0300

    fetch y componentDidMount

--- Basics/monsters-rolodex/src/App.js
+++ Basics/monsters-rolodex/src/App.js
@@ -6,22 +6,25 @@ class App extends Component {
     super();
 
     this.state = {
-      monters:[
-        { name: 'hola',
-          id: 'aa1'
-        }
-      ]
+      monsters:[]
     };
+  }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
   }
 
   render(){
     return (
       <div className="App">{
-        this.state.monters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
       }</div>
     );
   }
+
+  
 }
 
 export default App;
