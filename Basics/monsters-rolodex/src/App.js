bdee02cb1c2251f913ef5314183a3a81b618a673
Maximiliano Mascarello <48534691+mmascarello@users.noreply.github.com>
2021-09-16 17:47:40 -0300

    components and props
    props as children

--- Basics/monsters-rolodex/src/App.js
+++ Basics/monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor(){
@@ -18,9 +19,13 @@ class App extends Component {
 
   render(){
     return (
-      <div className="App">{
-        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-      }</div>
+      <div className="App">
+        <CardList>
+        { this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>) }
+        </CardList>
+
+        
+      </div>
     );
   }
 
