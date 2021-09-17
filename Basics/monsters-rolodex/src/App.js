d331dda3ef35f4430495b0bde75cafa929db79c7
Maximiliano Mascarello <48534691+mmascarello@users.noreply.github.com>
2021-09-17 11:58:02 -0300

    functional component

--- Basics/monsters-rolodex/src/App.js
+++ Basics/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
@@ -24,8 +25,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input type="search" placeholder="Search a monster" 
-        onChange={e => this.setState({ searchField : e.target.value}, () => console.log(this.state))}/>
+        <SearchBox 
+        placeholder="Search a monster"
+        handleChange = {e => this.setState({ searchField : e.target.value})}
+        />
+        
         <CardList monsters={filteredMonsters}/>
       </div>
     );
